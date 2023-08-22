1b99edcbd30ed760723dbbd1219193bcd183f29a
vlad777442 <vefirst@gmail.com>
2023-08-22 16:33:43 -0400

    added javadoc

--- deal/src/main/java/com/neoflex/deal/service/DealService.java
+++ deal/src/main/java/com/neoflex/deal/service/DealService.java
@@ -9,9 +9,25 @@ import com.neoflex.deal.model.Application;
 import java.util.List;
 
 public interface DealService {
+    /**
+     * This method creates Client and Application using LoanApplicationRequestDTO and returns offers
+     * @param requestDTO LoanApplicationRequestDTO
+     * @return 4 loan offers
+     */
     List<LoanOfferDTO> application(LoanApplicationRequestDTO requestDTO);
 
+    /**
+     * This method updates application using LoanOfferDTO
+     * @param loanOfferDTO LoanOfferDTO
+     * @return updated application
+     */
     Application updateApplication(LoanOfferDTO loanOfferDTO);
 
+    /**
+     * This method sends request to calculate credit details to Conveyor
+     * @param applicationId Long applicationId
+     * @param requestDTO FinishRegistrationRequestDTO
+     * @return credit details as CreditDTO
+     */
     CreditDTO calculateCreditByApplicationId(Long applicationId, FinishRegistrationRequestDTO requestDTO);
 }
